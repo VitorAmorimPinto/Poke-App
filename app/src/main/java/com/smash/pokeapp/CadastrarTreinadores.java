b863c0fb045c7306d128c1dab0641dda825f0ebc
Priscila Nascimento <fnas.priscila@gmail.com>
2021-09-30 20:12:45 -0300

    Layout cad treinadores

---
+++ app/src/main/java/com/smash/pokeapp/CadastrarTreinadores.java
@@ -0,0 +1,14 @@
+package com.smash.pokeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadastrarTreinadores extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar_treinadores);
+    }
+}
