2103262d8acdbb6078c70c5c717b0aa07e776ea7
Breno-SO <Breno.soliv@gmail.com>
2021-09-30 21:33:05 -0300

    branch Breno- commit de sincronização

---
+++ app/src/main/java/com/smash/pokeapp/AttTreinadorActivity.java
@@ -0,0 +1,14 @@
+package com.smash.pokeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AttTreinadorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_att_treinador);
+    }
+}
