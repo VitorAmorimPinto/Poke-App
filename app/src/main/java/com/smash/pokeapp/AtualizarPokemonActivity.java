70d7691952e7874d598982da777d5c6b31f9f0b9
Vitor Amorim <vitor232596.job@gmail.com>
2021-10-02 11:29:49 -0300

    branch vitor - Implementando exclusão de Pokémons e exibindo mensagens

--- app/src/main/java/com/smash/pokeapp/AtualizarPokemonActivity.java
+++ app/src/main/java/com/smash/pokeapp/AtualizarPokemonActivity.java
@@ -1,7 +1,9 @@
 package com.smash.pokeapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -29,6 +31,7 @@ public class AtualizarPokemonActivity extends AppCompatActivity {
 
 
 
+
     }
     public void preencherCampos(){
         edtNome.setText(p.getNome());
@@ -38,22 +41,83 @@ public class AtualizarPokemonActivity extends AppCompatActivity {
         edtHabilidade.setText(p.getHabilidade());
 
     }
-    public void submit(View view){
+    public void submit(View view) {
+        String mensagem = "";
         p.setNome("" + edtNome.getText());
         p.setPeso(Double.parseDouble(String.valueOf(edtPeso.getText())));
         p.setTipo("" + edtTipo.getText());
         p.setEspecie("" + edtEspecie.getText());
         p.setHabilidade("" + edtHabilidade.getText());
-    try {
-        BaseDados.rPokemon.update(p);
-        Toast.makeText(this, "Pokémon Atualizado com sucesso ", Toast.LENGTH_LONG).show();
-        Intent i = new Intent(this,ExibirPokemonsActivity.class);
-        startActivity(i);
-    }catch (Exception ex){
+        try {
+            BaseDados.rPokemon.update(p);
+            mensagem = "Pokémon atualizado com sucesso!";
 
+        } catch (Exception ex) {
+            mensagem = "Erro ao atualizar pokémon. Tente novamente mais tarde.";
+        }
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(AtualizarPokemonActivity.this);
+        alert.setTitle("Mensagem");
+        alert.setMessage(mensagem);
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                finish();
+            }
+        });
+        alert.show();
     }
 
+    public void excluir(View view){
 
+        if(p != null){
+            AlertDialog.Builder alert = new AlertDialog.Builder(AtualizarPokemonActivity.this);
+            alert.setTitle("Excluir Pokémon");
+            alert.setMessage("Deseja realmente excluir esse Pokémon?");
+            alert.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    String mensagem = "";
+
+                    try {
+                        BaseDados.rPokemon.update(p);
+                        mensagem = "Pokémon excluido com sucesso!";
+
+                    } catch (Exception ex) {
+                        mensagem = "Erro ao excluir pokémon. Tente novamente mais tarde.";
+                    }
+                    BaseDados.rPokemon.remove(p);
+                    dialog.dismiss();
+
+                    AlertDialog.Builder alerta = new AlertDialog.Builder(AtualizarPokemonActivity.this);
+                    alerta.setTitle("Mensagem");
+                    alerta.setMessage(mensagem);
+                    alerta.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                            finish();
+                        }
+                    });
+                    alerta.show();
+                }
+            });
+
+            alert.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+
+            alert.show();
+        }
     }
+
 
 }
